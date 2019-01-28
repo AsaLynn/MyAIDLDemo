05d01d07e257a138f814cc9017abb3451d62481b
zxn <zhangxiaoning@avicare.cn>
2019-01-28 14:26:37 +0800

    增加输出服务端线程名称

--- app/src/main/java/com/zxn/service/service/OnroadService.java
+++ app/src/main/java/com/zxn/service/service/OnroadService.java
@@ -1,16 +1,24 @@
 package com.zxn.service.service;
 
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 import android.os.RemoteException;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.zxn.service.IOnroad;
 
 public class OnroadService extends Service {
-
     public OnroadService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
     }
 
     private String TAG = "OnroadService";
@@ -18,10 +26,15 @@ public class OnroadService extends Service {
 
         @Override
         public void sayHello(String name, int age) throws RemoteException {
-            String text = "sayHello: --->name" + name + "age:" + age;
+            //--->name:say onClickage:1028Thread:Binder:5786_4
+            final String text = "sayHello: --->name:" + name + "age:" + age + "Thread:" + Thread.currentThread().getName();
             Log.i(TAG, text);
-            //Toast.makeText(OnroadService.this, text, Toast.LENGTH_SHORT).show();
-
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(OnroadService.this, text, Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     };
 
@@ -29,4 +42,7 @@ public class OnroadService extends Service {
     public IBinder onBind(Intent intent) {
         return binder;
     }
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler();
 }
--- myclient/src/main/java/com/zxn/client/MainActivity.java
+++ myclient/src/main/java/com/zxn/client/MainActivity.java
@@ -7,20 +7,21 @@ import android.os.IBinder;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.zxn.service.IOnroad;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ServiceConnection connection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
-           // IOnroad.Stub binder =
-            IOnroad onroad = IOnroad.Stub.asInterface(service);
+            // IOnroad.Stub binder =
+            mOnroad = IOnroad.Stub.asInterface(service);
             try {
 //                onroad.sayHello();
-                onroad.sayHello("哈喽",100);
+                mOnroad.sayHello("哈喽", 100);
             } catch (RemoteException e) {
                 e.printStackTrace();
                 Toast.makeText(MainActivity.this, "有异常的情况!", Toast.LENGTH_SHORT).show();
@@ -29,17 +30,42 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
-
+            mOnroad = null;
         }
     };
+    private IOnroad mOnroad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        service();
+        findViewById(R.id.tv_call_service).setOnClickListener(this);
+    }
+
+    private void service() {
         Intent intent = new Intent();
         intent.setAction("com.lypeer.aidl");
         intent.setPackage("com.zxn.service");
-        bindService(intent,connection,BIND_AUTO_CREATE);
+        bindService(intent, connection, BIND_AUTO_CREATE);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        try {
+            if (null != mOnroad) {
+                mOnroad.sayHello("say onClick", 1028);
+            } else {
+                service();
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "调用出现异常情况!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
